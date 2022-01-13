825b0f91b8bdd2485063275cfc4da9c2d642d541
NT.Alexander <84801914+alexandernt@users.noreply.github.com>
2022-01-13 20:01:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let personas = [
+    "Zaira",
+    "Jorge",
+    "Marta",
+    "Pedro",
+    "Maria"
+];
+
+let cosas = [
+    "Teclado",
+    "Mesa",
+    "Mouse",
+    "Jarra" 
+];
+
+const ordenarArrays = (x) => {
+    return x.sort();
+};
+
+console.log(ordenarArrays(personas));
+console.log(ordenarArrays(cosas));
